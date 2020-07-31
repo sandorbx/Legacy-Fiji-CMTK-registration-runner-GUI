e899465854e004b8e12001e823af33f9e8a68812
sandorbx <62243931+sandorbx@users.noreply.github.com>
2020-08-01 00:00:29 +0200

    Update wslwin10_install.sh

--- wslwin10_install.sh
+++ wslwin10_install.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 sudo apt update
-sudo apt upgrade
-sudo apt install cmtk
-sudo apt install pcmanfm
-sudo apt install mousepad
-sudo apt install unzip
+sudo apt upgrade -y
+sudo apt install cmtk -y
+sudo apt install pcmanfm -y
+sudo apt install mousepad -y
+sudo apt install unzip -y
+cd ~/
 wget "https://downloads.imagej.net/fiji/latest/fiji-linux64.zip"
 unzip fiji-linux64.zip
-sudo chmod -R a+rwx ~/Fiji-CMTK-registration-runner-GUI/cmtk_munger_wsl_linux
-cp ~/Fiji-CMTK-registration-runner-GUI/Fijij_CMTK_registration_runner.ijm ~/Fiji.app/plugins/Macros
-cp -r ~/Fiji-CMTK-registration-runner-GUI/cmtk ~/Fiji.app/lib/
-sudo chmod -R a+rwx ~/Fiji.app/lib/cmtk
+git clone https://github.com/sandorbx/Fiji-CMTK-registration-runner-GUI.git
+sudo chmod -R a+rwx ~/Fiji-CMTK-registration-runner-GUI/
+cp ~/Fiji-CMTK-registration-runner-GUI/Fiji_CMTK_registration_runner.ijm ~/Fiji.app/plugins/Macros
+cp -r ~/Fiji-CMTK-registration-runner-GUI/cmtk_munger_wsl_linux ~/Fiji.app/lib/
+sudo chmod -R a+rwx ~/Fiji.app/lib/cmtk_munger_wsl_linux
+rm -r ~/Fiji-CMTK-registration-runner-GUI/
 rm -r ~/fiji-linux64.zip
+~/Fiji.app/ImageJ-linux64
