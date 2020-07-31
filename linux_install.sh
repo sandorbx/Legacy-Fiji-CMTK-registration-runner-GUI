e0e74050b16964c3e6b6df59a2246d8461f5e9b5
sandorbx <62243931+sandorbx@users.noreply.github.com>
2020-07-31 14:52:28 +0200

    install scripts

--- linux_install.sh
+++ linux_install.sh
@@ -2,8 +2,6 @@
 sudo apt update
 sudo apt upgrade
 sudo apt install cmtk
-sudo apt install pcmanfm
-sudo apt install mousepad
 sudo apt install unzip
 sudo apt install git
 cd ~/
@@ -11,7 +9,7 @@ wget "https://downloads.imagej.net/fiji/latest/fiji-linux64.zip"
 unzip fiji-linux64.zip
 git clone https://github.com/sandorbx/Fiji-CMTK-registration-runner-GUI.git
 sudo chmod -R a+rwx ~/Fiji-CMTK-registration-runner-GUI/cmtk_munger_wsl_linux
-cp ~/Fiji-CMTK-registration-runner-GUI/Fijij_CMTK_registration_runner.ijm ~/Fiji.app/plugins/Macros
+cp ~/Fiji-CMTK-registration-runner-GUI/Fiji_CMTK_registration_runner.ijm ~/Fiji.app/plugins/Macros
 cp -r ~/Fiji-CMTK-registration-runner-GUI/cmtk ~/Fiji.app/lib/
 sudo chmod -R a+rwx ~/Fiji.app/lib/cmtk
 rm -r ~/Fiji-CMTK-registration-runner-GUI/cmtk_munger_wsl_linux
