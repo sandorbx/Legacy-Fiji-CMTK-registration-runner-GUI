5badb6e1e6b08c98cc49f4c3a7f009fb25430923
sandorbx <62243931+sandorbx@users.noreply.github.com>
2020-08-01 19:20:52 +0200

    Update linux_install.sh

--- linux_install.sh
+++ linux_install.sh
@@ -7,11 +7,12 @@ sudo apt install git -y
 mkdir ~/regrunnertemp
 cd ~/regrunnertemp
 wget "https://downloads.imagej.net/fiji/latest/fiji-linux64.zip"
-unzip fiji-linux64.zip -d ~/
+unzip fiji-linux64.zip -d ~/Desktop
 git clone https://github.com/sandorbx/Fiji-CMTK-registration-runner-GUI.git
 sudo chmod -R a+rwx ~/regrunnertemp/Fiji-CMTK-registration-runner-GUI/
-cp ~/regrunnertemp/Fiji-CMTK-registration-runner-GUI/Fiji_CMTK_registration_runner.ijm ~/Fiji.app/plugins/Macros
-cp -r ~/regrunnertemp/Fiji-CMTK-registration-runner-GUI/cmtk_munger_wsl_linux ~/Fiji.app/lib/
-sudo chmod -R a+rwx ~/Fiji.app/lib/cmtk_munger_wsl_linux
+cp ~/regrunnertemp/Fiji-CMTK-registration-runner-GUI/Fiji_CMTK_registration_runner.ijm ~/Desktop/Fiji.app/plugins/Macros
+cp -r ~/regrunnertemp/Fiji-CMTK-registration-runner-GUI/cmtk_munger_wsl_linux ~/Desktop/Fiji.app/lib/
+sudo chmod -R a+rwx ~/Desktop/Fiji.app/lib/cmtk_munger_wsl_linux
 rm -rf ~/regrunnertemp
+cd ~/Desktop/Fiji.app
 ~/Fiji.app/ImageJ-linux64
