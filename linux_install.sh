32dde32ec2a0e75b08a988b6658d131c3dab1c1c
sandorbx <62243931+sandorbx@users.noreply.github.com>
2020-08-01 15:42:27 +0200

    improved installer

--- linux_install.sh
+++ linux_install.sh
@@ -2,16 +2,16 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install cmtk -y
-sudo apt install unzip 
+sudo apt install unzip -y
 sudo apt install git -y
-cd ~/
+mkdir ~/regrunnertemp
+cd ~/regrunnertemp
 wget "https://downloads.imagej.net/fiji/latest/fiji-linux64.zip"
-unzip fiji-linux64.zip
+unzip fiji-linux64.zip -d ~/
 git clone https://github.com/sandorbx/Fiji-CMTK-registration-runner-GUI.git
-sudo chmod -R a+rwx ~/Fiji-CMTK-registration-runner-GUI/
-cp ~/Fiji-CMTK-registration-runner-GUI/Fiji_CMTK_registration_runner.ijm ~/Fiji.app/plugins/Macros
-cp -r ~/Fiji-CMTK-registration-runner-GUI/cmtk_munger_wsl_linux ~/Fiji.app/lib/
+sudo chmod -R a+rwx ~/regrunnertemp/Fiji-CMTK-registration-runner-GUI/
+cp ~/regrunnertemp/Fiji-CMTK-registration-runner-GUI/Fiji_CMTK_registration_runner.ijm ~/Fiji.app/plugins/Macros
+cp -r ~/regrunnertemp/Fiji-CMTK-registration-runner-GUI/cmtk_munger_wsl_linux ~/Fiji.app/lib/
 sudo chmod -R a+rwx ~/Fiji.app/lib/cmtk_munger_wsl_linux
-rm -r ~/Fiji-CMTK-registration-runner-GUI/
-rm -r ~/fiji-linux64.zip
+rm -rf ~/regrunnertemp
 ~/Fiji.app/ImageJ-linux64
--- wslwin10_install.sh
+++ wslwin10_install.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-sudo apt update
-sudo apt upgrade -y
 sudo apt install cmtk -y
 sudo apt install pcmanfm -y
 sudo apt install mousepad -y
@@ -8,10 +6,9 @@ sudo apt install unzip -y
 cd ~/
 wget "https://downloads.imagej.net/fiji/latest/fiji-linux64.zip"
 unzip fiji-linux64.zip
-git clone https://github.com/sandorbx/Fiji-CMTK-registration-runner-GUI.git
 sudo chmod -R a+rwx ~/Fiji-CMTK-registration-runner-GUI/
 cp ~/Fiji-CMTK-registration-runner-GUI/Fiji_CMTK_registration_runner.ijm ~/Fiji.app/plugins/Macros
 cp -r ~/Fiji-CMTK-registration-runner-GUI/cmtk_munger_wsl_linux ~/Fiji.app/lib/
 sudo chmod -R a+rwx ~/Fiji.app/lib/cmtk_munger_wsl_linux
-rm -r ~/Fiji-CMTK-registration-runner-GUI/
-rm -r ~/fiji-linux64.zip
+rm -rf ~/Fiji-CMTK-registration-runner-GUI/
+rm -rf ~/fiji-linux64.zip
