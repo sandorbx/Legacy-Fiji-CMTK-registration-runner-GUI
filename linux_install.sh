f0fb999829846ef34d0aa05d734ab80aa3b462b9
sandorbx <62243931+sandorbx@users.noreply.github.com>
2020-08-02 12:24:57 +0200

    Update linux_install.sh

--- linux_install.sh
+++ linux_install.sh
@@ -4,6 +4,7 @@ sudo apt upgrade -y
 sudo apt install cmtk -y
 sudo apt install unzip -y
 sudo apt install git -y
+sudo apt install default-jre -y
 mkdir ~/regrunnertemp
 cd ~/regrunnertemp
 git clone https://github.com/sandorbx/Fiji-CMTK-registration-runner-GUI.git
